69b72d0063611f355a8e0a9fd41ce58b7d3ce19e
NicKAnderson038 <andersonnick038@gmail.com>
2019-02-23 21:22:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -18,11 +18,11 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+          </a> */}
+                </header>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
